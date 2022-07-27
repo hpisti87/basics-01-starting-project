a4f80b02e9c0ed62bd039feefd5705a3b940fbb4
hpisti87 <pisti87@gmail.com>
2022-07-27 20:36:35 +0200

    Section 2: 26

---
+++ assets/scripts/app.js
@@ -0,0 +1,8 @@
+const defaultResult = 0;
+let currentResult = defaultResult;
+
+currentResult += 10;
+
+let calculationDescription = '(0 + 10)';
+
+outputResult(currentResult, calculationDescription);
