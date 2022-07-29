5cd4e850985b6fa5023dcf89283e59753c6b07e5
hpisti87 <pisti87@gmail.com>
2022-07-29 18:24:17 +0200

    Section 2: 35

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,13 +1,12 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-function add(num1, num2) {
-  const result = num1 + num2;
-  return result;
+function add() {
+  currentResult = currentResult + userInput.value;
+  outputResult(currentResult, '');
 }
 
-currentResult = add(1, 2);
+addBtn.addEventListener('click', add);
 
 let calculationDescription = `(${defaultResult} + 10)`;
 
-outputResult(currentResult, calculationDescription);
